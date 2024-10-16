531dd8da478e744686f1f98e8f22bc11c6151065
RickOliwer <rickoliwer@gmail.com>
2024-10-16 20:05:42 +0200

    update: error handeling fetch function

--- src/api/transactions.ts
+++ src/api/transactions.ts
@@ -5,46 +5,66 @@ type Item = {
   amount: number;
 };
 
-const LIMIT = 10;
+interface TransactionsRes {
+  data: Item[];
+  total: number;
+}
 
 export async function fetchTransactions({
   page,
   start,
   end,
+  size = 10,
 }: {
   page: number;
   start?: string;
   end?: string;
+  size?: number;
 }): Promise<{
   transactions: Item[];
   total: number;
   hasMore: boolean;
   totalPages: number;
 }> {
-  const res = await fetch(
-    `http://localhost:8080/api/transactions?page=${page}&size=${LIMIT}`,
-    {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
+  try {
+    const params = new URLSearchParams({
+      page: page.toString(),
+      size: size.toString(),
+    });
+
+    if (start) params.append("start", start);
+    if (end) params.append("end", end);
+
+    const res = await fetch(
+      `http://localhost:8080/api/transactions?${params.toString()}`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    if (!res.ok) {
+      throw new Error(
+        `Failed to fetch data from api, statusCode: ${res.status})`
+      );
     }
-  );
 
-  const data = await res.json();
+    const data: TransactionsRes = await res.json();
 
-  const totalPages = Math.ceil(data?.total / LIMIT);
+    const totalPages = Math.ceil(data.total / size);
+    const hasMore = page < totalPages - 1;
 
-  const hasMore = page < totalPages - 1;
+    return {
+      transactions: data.data,
+      total: data.total,
+      hasMore,
+      totalPages,
+    };
+  } catch (error) {
+    console.error("Error fetching transactions:", error);
 
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve({
-        transactions: data?.data,
-        total: data?.total,
-        hasMore: hasMore,
-        totalPages: totalPages,
-      });
-    }, 1000);
-  });
+    throw error;
+  }
 }
